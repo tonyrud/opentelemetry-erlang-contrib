fb82b93801f1a117139f56dfabb84729fd106cfa
tonyrud <tonrudny@gmail.com>
2025-08-11 21:47:41 -0400

    load gen random

---
+++ examples/roll_dice/load.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "\nRunning script:\n$PWD/$0\n"
+
+# -e, -o errexit: Exit on error.
+set -e
+
+BASE_URL=${BASE_URL:-"http://localhost:4000"}
+
+URLS=(
+  "/random"
+  "/api/rolldice"
+)
+URLS_LEN=${#URLS[@]}
+
+while true; do
+  # shellcheck disable=SC2004
+  URL_IDX=$((${RANDOM} % ${URLS_LEN}))
+  URL="${BASE_URL}${URLS[$URL_IDX]}"
+  echo "calling ${URL}"
+  curl -k -s  "${URL}" > /dev/null
+
+  sleep "$(echo "scale=4;${URL_IDX} / ${URLS_LEN}0 " | bc)"
+done
